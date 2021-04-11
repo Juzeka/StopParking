71c8e7bb7226e8e65c8658f63169b45f8457c95c
Juzeka <rafaelgomesalmeida@hotmail.com>
2021-04-11 12:03:09 -0300

    melhorando o layout

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-#ALLOWED_HOSTS = ['stopparking.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stopparking.herokuapp.com']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
